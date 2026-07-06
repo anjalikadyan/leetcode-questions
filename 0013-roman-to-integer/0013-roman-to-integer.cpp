f69abfe086998310f091310bb7fecd7c1dce27e7
Anjali Kadyan <anjalikadyan607@gmail.com>
2026-07-06 11:03:32 +0530

    Time: 118 ms (5.73%), Space: 13.4 MB (17.95%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,32 @@
+#include <map>
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> age;
+        age['I'] = 1;
+        age['V'] = 5;
+        age['X'] = 10;
+        age['L'] = 50;
+        age['C'] = 100;
+        age['D'] = 500;
+        age['M'] = 1000;
+        int sum=0;
+        char pre=s[0];
+        for(char a:s){
+            // sum+=age[a];
+            std::cout<<sum<<" ";
+            if(age[pre]<age[a]){
+                sum+=age[a];
+                int d=2*age[pre];
+                sum-=d;
+
+            }
+            else if(age[pre]>=age[a]){
+                sum+=age[a];
+            }
+            pre=a;
+            std::cout<<pre<<" ";
+        }
+        return sum;
+    }
+};
